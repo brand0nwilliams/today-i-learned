4325966a7439830f54eeb62cc7dbe4ffac137f67
Brandon Williams <brandonwilliams@Brandons-MacBook-Pro.local>
2024-12-22 18:45:26 -0500

    Fetch data from Supabase. Map categories to color codes.

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-console.log("hello world");
-const initialFacts = [
-  {
-    id: 1,
-    text: "React is being developed by Meta (formerly facebook)",
-    source: "https://opensource.fb.com/",
-    category: "technology",
-    votesInteresting: 24,
-    votesMindblowing: 9,
-    votesFalse: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-    source:
-      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
-    votesInteresting: 11,
-    votesMindblowing: 2,
-    votesFalse: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: "Lisbon is the capital of Portugal",
-    source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
-    votesInteresting: 8,
-    votesMindblowing: 3,
-    votesFalse: 1,
-    createdIn: 2015,
-  },
-];
-
 const CATEGORIES = [
   {
     name: "technology",
@@ -51,14 +16,36 @@ const CATEGORIES = [
 ];
 
 // Selecting DOM Elements
-const btn = document.querySelector(".btn-open");
-const form = document.querySelector(".fact-form");
-const factsList = document.querySelector(".facts-list");
+const btn = document.querySelector(
+  ".btn-open"
+);
+const form = document.querySelector(
+  ".fact-form"
+);
+const factsList =
+  document.querySelector(".facts-list");
 
 // Create DOM elements: Render facts in list
 factsList.innerHTML = "";
-createFactsList(initialFacts);
 
+// Load data from Supbase
+async function loadFacts() {
+  const res = await fetch(
+    "https://fnbpbvwcuufabmcapdfa.supabase.co/rest/v1/facts",
+    {
+      headers: {
+        apikey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZuYnBidndjdXVmYWJtY2FwZGZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ4OTI2ODIsImV4cCI6MjA1MDQ2ODY4Mn0.511Nwq1lN5ZGh9xqn4U4p5lKHnSZ52ArN386s9OQoJM",
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZuYnBidndjdXVmYWJtY2FwZGZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ4OTI2ODIsImV4cCI6MjA1MDQ2ODY4Mn0.511Nwq1lN5ZGh9xqn4U4p5lKHnSZ52ArN386s9OQoJM",
+      },
+    }
+  );
+  const data = await res.json(); // await keyword used for functions that return promises
+  createFactsList(data);
+}
+
+loadFacts();
 function createFactsList(dataArray) {
   const htmlArray = dataArray.map(
     (fact) => `<li class="fact">
@@ -66,24 +53,38 @@ function createFactsList(dataArray) {
                         ${fact.text}
                         <a
                           class="source"
-                          href=${fact.source}
+                          href=${
+                            fact.source
+                          }
                           target="_blank"
                           >(Source)</a
                         >
                       </p>
                       <!--- "span" tag creates a new text element without a new line, allows for separate styling -->
-                      <span class="tag" style="background-color: #3b82f6"
-                        >${fact.category}</span
+                      <span class="tag" style="background-color: ${
+                        CATEGORIES.find(
+                          (category) =>
+                            category.name ===
+                            fact.category
+                        ).color
+                      }"
+                        >${
+                          fact.category
+                        }</span
                       ></li>`
   );
 
-  console.log(htmlArray);
   const html = htmlArray.join("");
-  factsList.insertAdjacentHTML("afterbegin", html);
+  factsList.insertAdjacentHTML(
+    "afterbegin",
+    html
+  );
 }
 
 btn.addEventListener("click", () => {
-  if (form.classList.contains("hidden")) {
+  if (
+    form.classList.contains("hidden")
+  ) {
     form.classList.remove("hidden");
     btn.textContent = "Close";
   } else {
