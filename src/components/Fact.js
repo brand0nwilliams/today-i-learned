f3035cdb2c461d77b1cb906def82678ed7b64ccc
brand0nwilliams <bpwilliams13@gmail.com>
2024-12-28 16:43:03 -0500

    Refactor - CateogryFilter, Fact, FactList Component files

---
+++ src/components/Fact.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import supabase from "../supabase";
+import { CATEGORIES } from "../constants";
+
+function Fact({ fact, setFacts }) {
+  const [isUpdating, setIsUpdating] =
+    useState(false);
+
+  async function handleVote(
+    columnName
+  ) {
+    setIsUpdating(true);
+    const { data: updatedFact, error } =
+      await supabase
+        .from("facts")
+        .update({
+          [columnName]:
+            fact[columnName] + 1,
+        })
+        .eq("id", fact.id)
+        .select();
+
+    setIsUpdating(false);
+    if (!error) {
+      setFacts((facts) =>
+        facts.map((f) =>
+          f.id === fact.id
+            ? updatedFact[0]
+            : f
+        )
+      );
+    }
+  }
+
+  return (
+    <li className="fact">
+      <p>
+        {fact.text}
+        <a
+          className="source"
+          href={fact.source}
+          target="_blank"
+        >
+          (Source)
+        </a>
+      </p>
+      <span
+        className="tag"
+        style={{
+          backgroundColor:
+            CATEGORIES.find(
+              (category) =>
+                category.name ===
+                fact.category
+            ).color,
+        }}
+      >
+        {fact.category}
+      </span>
+      <div className="vote-button">
+        <button
+          onClick={() =>
+            handleVote(
+              "votesInteresting"
+            )
+          }
+          disabled={isUpdating}
+        >
+          👍 {fact.votesInteresting}
+        </button>
+        <button
+          onClick={() =>
+            handleVote(
+              "votesMindblowing"
+            )
+          }
+          disabled={isUpdating}
+        >
+          🤯 {fact.votesMindblowing}
+        </button>
+        <button
+          onClick={() =>
+            handleVote("votesFalse")
+          }
+          disabled={isUpdating}
+        >
+          ⛔️ {fact.votesFalse}
+        </button>
+      </div>
+    </li>
+  );
+}
+
+export default Fact;
