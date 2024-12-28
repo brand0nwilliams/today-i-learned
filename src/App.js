7e5468697626e5916b08623445bc96e0556d2897
brand0nwilliams <bpwilliams13@gmail.com>
2024-12-28 16:17:48 -0500

    Refactor - Loader, NewFactForm Component files

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import React, {
 import supabase from "./supabase";
 import "./style.css";
 import Header from "./components/Header";
+import Loader from "./components/Loader";
+import NewFactForm from "./components/NewFactForm";
 
 const initialFacts = [
   {
@@ -143,14 +145,6 @@ function App() {
   );
 }
 
-function Loader() {
-  return (
-    <p className="message">
-      Loading...
-    </p>
-  );
-}
-
 const CATEGORIES = [
   {
     name: "technology",
@@ -167,120 +161,6 @@ const CATEGORIES = [
   { name: "history", color: "#f97316" },
   { name: "news", color: "#8b5cf6" },
 ];
-
-function NewFactForm({
-  setFacts,
-  setShowForm,
-}) {
-  const [text, setText] = useState("");
-  const [source, setSource] =
-    useState("");
-  const [category, setCategory] =
-    useState("");
-  const [isUploading, setIsUploading] =
-    useState(false);
-  const textLength = text.length;
-
-  function isValidHttpUrl(string) {
-    let url;
-
-    try {
-      url = new URL(string);
-    } catch (_) {
-      return false;
-    }
-
-    return (
-      url.protocol === "http:" ||
-      url.protocol === "https:"
-    );
-  }
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    console.log(text, source, category);
-
-    if (
-      text &&
-      isValidHttpUrl(source) &&
-      category &&
-      textLength <= 200
-    ) {
-      setIsUploading(true);
-      const { data: newFact, error } =
-        await supabase
-          .from("facts")
-          .insert([
-            { text, source, category },
-          ])
-          .select();
-
-      setIsUploading(true);
-      if (!error)
-        setFacts((facts) => [
-          newFact[0],
-          ...facts,
-        ]);
-
-      setText("");
-      setSource("");
-      setCategory("");
-      setShowForm(false);
-    }
-  }
-
-  return (
-    <form
-      className="fact-form"
-      onSubmit={handleSubmit}
-    >
-      <input
-        type="text"
-        placeholder="Share a fact with the world..."
-        value={text}
-        onChange={(e) =>
-          setText(e.target.value)
-        }
-        disabled={isUploading}
-      />
-      <span>{200 - textLength}</span>
-      <input
-        type="text"
-        placeholder="Trustworthy source..."
-        value={source}
-        onChange={(e) =>
-          setSource(e.target.value)
-        }
-        disabled={isUploading}
-      />
-      <select
-        value={category}
-        onChange={(e) =>
-          setCategory(e.target.value)
-        }
-        disabled={isUploading}
-      >
-        <option value="">
-          Choose category:
-        </option>
-        {CATEGORIES.map((cat) => (
-          <option
-            key={cat.name}
-            value={cat.name}
-          >
-            {cat.name.toUpperCase()}
-          </option>
-        ))}
-      </select>
-      <button
-        className="btn btn-large"
-        disabled={isUploading}
-      >
-        Post
-      </button>
-    </form>
-  );
-}
 
 function CategoryFilter({
   setCurrentCategory,
